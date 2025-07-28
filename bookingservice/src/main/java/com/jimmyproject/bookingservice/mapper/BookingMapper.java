2a7bd00a55a2a87de54284c810be55c4c87af418
jimmy <jimoseipeace2bb@gmail.com>
2025-07-28 00:30:02 -0500

    assign port to the services:

--- bookingservice/src/main/java/com/jimmyproject/bookingservice/mapper/BookingMapper.java
+++ bookingservice/src/main/java/com/jimmyproject/bookingservice/mapper/BookingMapper.java
@@ -6,14 +6,9 @@ import com.jimmyproject.bookingservice.entity.Booking;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", 
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface BookingMapper {
     BookingResponseDto toBookingResponseDto(Booking booking);
-    
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
     Booking toEntity(BookingRequestDto bookingRequestDto);
     
     @Mapping(target = "id", ignore = true)
